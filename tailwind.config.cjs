7d8c5e346b7e28cdd38368cf58a37ce78e59bc47
qievenz <ivann96@gmail.com>
2025-12-04 20:59:07 -0300

    enhance styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				background: '#111827',
+				primary: '#1F2937',
+				'secondary-text': '#D1D5DB',
+				'secondary-heading': '#F9FAFB',
+				accent: '#22D3EE',
+			},
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 }
